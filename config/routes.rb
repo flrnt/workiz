3792aeb209e71bc58e2008b256cc94d520fed465
Florent Allain <allain.florent@gmail.com>
2015-09-22 16:14:09 +0200

    add routes for book

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :book
   root to: 'page#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
