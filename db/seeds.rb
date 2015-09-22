d5a794eabbbeb1bd61977b06273463b328f55ab4
Florent Allain <allain.florent@gmail.com>
2015-09-22 16:45:48 +0200

    fix faker name issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,16 @@
 require 'faker'
 
+Book.destroy_all
 # Creating 40 fake books
 40.times do
   book = Book.new({
-    author: Faker::Book.author,
+    author: Faker::Name.name,
     title: Faker::Book.title,
   })
   book.save
 end
 
-
+User.destroy_all
 # Creating 40 fake users
 40.times do
   user = User.new({
