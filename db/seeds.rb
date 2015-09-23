732c7cdb8f31ce9d7e9429849f31148b8de09141
Florent Allain <allain.florent@gmail.com>
2015-09-23 16:21:04 +0200

    populated seed with collection

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 require 'faker'
 
+
+Collection.destroy_all
+
 Book.destroy_all
-# Creating 40 fake books
 40.times do
   book = Book.new({
     author: Faker::Name.name,
@@ -10,27 +12,35 @@ Book.destroy_all
   book.save
 end
 
-# User.destroy_all
-# User.reset_pk_sequence
-# # Creating 40 fake users
-# 40.times do
-#   user = User.new({
-#     email: Faker::Internet.email,
-#     password: "supermotdepasse1",
-#     password_confirmation: "supermotdepasse1",
-#     current_sign_in_at: Time.now,
-#     last_sign_in_at: Time.now,
-#     created_at: Time.now,
-#     updated_at: Time.now,
-#   })
-#   user.save!
-# end
+User.destroy_all
+40.times do
+  user = User.new({
+    email: Faker::Internet.email,
+    password: "supermotdepasse1",
+    password_confirmation: "supermotdepasse1",
+    current_sign_in_at: Time.now,
+    last_sign_in_at: Time.now,
+    created_at: Time.now,
+    updated_at: Time.now,
+  })
+  user.save!
+end
 
+u_ids = []
+User.find_each do |user|
+  u_ids << user.id
+end
 
-  # collection = Collection.new({
-  #   user_id:2,
-  #   book_id:8,
-  #   available:true,
-  # })
-  # collection.save
+b_ids = []
+Book.find_each do |book|
+  b_ids << book.id
+end
 
+40.times do
+  collection = Collection.new({
+    user_id: u_ids.sample,
+    book_id:b_ids.sample,
+    available:true,
+  })
+  collection.save
+end
