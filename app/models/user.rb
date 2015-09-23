ec1eb55c9f8f241169016e128d6208eb9ebe16cb
Florent Allain <allain.florent@gmail.com>
2015-09-23 16:27:57 +0200

    add has_many through relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :collections
+  has_many :books, through: :collections
   has_many :borrows
 end
