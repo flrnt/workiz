5980eb7d3a383c4b74c91c2a126d9d5ee8b4caf9
Florent Allain <allain.florent@gmail.com>
2015-09-23 17:07:34 +0200

    add relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :collections
   has_many :books, through: :collections
   has_many :borrows
 end
