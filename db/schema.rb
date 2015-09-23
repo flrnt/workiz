6314e34fc4da51d91d1eb0cb825a4079e0597fdc
Florent Allain <allain.florent@gmail.com>
2015-09-23 20:53:29 +0200

    add description to books

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922140937) do
+ActiveRecord::Schema.define(version: 20150923185306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,9 @@ ActiveRecord::Schema.define(version: 20150922140937) do
   create_table "books", force: :cascade do |t|
     t.string   "author"
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "borrows", force: :cascade do |t|
