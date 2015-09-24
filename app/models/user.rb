9551938c2b36f06f8a4dc3e17aadf57fde9d1600
Florent Allain <allain.florent@gmail.com>
2015-09-24 20:34:25 +0200

    add has_many relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :collections
   has_many :books, through: :collections
   has_many :borrows
 end
