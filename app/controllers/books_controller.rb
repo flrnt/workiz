95cc16b63216f8bb600d449415795a562868b8e3
Florent Allain <allain.florent@gmail.com>
2015-09-24 16:28:39 +0200

    update policy controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,20 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
   def index
-    @books = Book.all
+    @books = policy_scope(Book)
   end
   def show
     @owner = User.find(Collection.where(book_id:@book.id)[0][:user_id])
   end
   def new
     @book = Book.new
+    authorize @book
     @collection = Collection.new
+    authorize @collection
   end
   def create
     @book = Book.create(book_params)
+    authorize @book
     @collection = Collection.new(book_id: @book.id, user_id: current_user.id, available: true)
     @collection.save
     redirect_to books_path
@@ -30,6 +33,7 @@ class BooksController < ApplicationController
   private
   def set_book
     @book = Book.find(params[:id])
+    authorize @book
   end
   def book_params
     params.require(:book).permit(:author, :title, :description)
